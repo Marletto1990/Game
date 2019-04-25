40b2c7e444d182ad09ed2f41d91e63b3be183a48
С рабочего компа АртИнвест <20day02@rambler.ru>
2019-04-25 17:34:05 +0300

    Создал меню выбора персонажа. Нужно исправить в контроллере обработчик кнопки приращения статов

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -11,18 +11,52 @@ sap.ui.define([
             model1.loadData("model/FormData.json");
             this.getView().setModel(model1);
             var basicClassStatsTable = this.byId("basicClassStatsTable");
-            basicClassStatsTable.bindElement("/All_Classes/0/table_stats/0");
+            basicClassStatsTable.bindElement("/All_Classes/0");
             console.log(basicClassStatsTable);
         },
         onMenuSelect: function(oControlEvent) {
             //console.log(oControlEvent.getSource().sId);
             if (oControlEvent.mParameters.selected === true) {
-                MessageToast.show(oControlEvent.getSource().getBindingContext().getProperty('src') + ".jpg");
+                //MessageToast.show(oControlEvent.getSource().getBindingContext().getProperty('src') + ".jpg");
                 var sSelectedSrc = oControlEvent.getSource().getBindingContext().getProperty('src');
-                console.log(sSelectedSrc);
+                //console.log(sSelectedSrc);
                 this.getView().getModel().setProperty('/Image_Show/0/src', sSelectedSrc);
+
+                var selectedClass = oControlEvent.getSource().getBindingContext().getProperty('class');
+                var changeBasicClassStatsTable = this.byId("basicClassStatsTable");
+                var checker = 0;
+                var bindRoot;
+                switch (selectedClass) {
+                    case 'Warrior':
+                        checker = 0;
+                        break;
+                    case 'Wizard':
+                        checker = 1;
+                        break;
+                    case 'Paladin':
+                        checker = 2;
+                        break;
+                };
+                bindRoot = "/All_Classes/" + checker;
+                changeBasicClassStatsTable.bindElement(bindRoot);
+                //console.log(bindRoot);
+
+
             }
             //console.log(oControlEvent.getSource().getBindingContext().getProperty('src'));
+        },
+        startStatBonusButtonPlus: function(oEvent) {
+            let sPath = oEvent.getSource().getBindingContext().getPath();
+
+            let sStartValue = oEvent.getSource().getBindingContext().getProperty('start_value');
+            let sStartStatBonus = oEvent.getSource().getBindingContext().getProperty('start_stat_bonus');
+            let sCurrentStat = oEvent.getSource().getBindingContext().getProperty('current_value');
+
+            this.getView().getModel().setProperty(sPath + '/current_value', +sStartValue + (+sStartStatBonus + 1));
+            this.getView().getModel().setProperty(sPath + '/start_stat_bonus', +sStartStatBonus + 1);
+
+            console.log(oEvent.getSource().getBindingContext().getProperty('start_stat_bonus'));
+            console.log(sPath + '/current_value', 1 + sStartValue + sStartStatBonus);
         },
         onImageLoad: function() {
             //MessageToast.show("Картинка загрузилась");
