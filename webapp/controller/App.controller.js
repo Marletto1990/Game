533f9118702f47194e106a5a0fd922b26f9be411
Evgen <20day02@rambler.ru>
2019-04-27 00:07:36 +0300

    Панель + -
    добавлен инкремент и декремент для кнопок уменьшить и увеличить характеристику. Не решена проблема с кнопкой уменьшить

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -19,7 +19,7 @@ sap.ui.define([
             if (oControlEvent.mParameters.selected === true) {
                 //MessageToast.show(oControlEvent.getSource().getBindingContext().getProperty('src') + ".jpg");
                 var sSelectedSrc = oControlEvent.getSource().getBindingContext().getProperty('src');
-                //console.log(sSelectedSrc);
+
                 this.getView().getModel().setProperty('/Image_Show/0/src', sSelectedSrc);
 
                 var selectedClass = oControlEvent.getSource().getBindingContext().getProperty('class');
@@ -49,14 +49,39 @@ sap.ui.define([
             let sPath = oEvent.getSource().getBindingContext().getPath();
 
             let sStartValue = oEvent.getSource().getBindingContext().getProperty('start_value');
+            let sStartStatBonus = oEvent.getSource().getBindingContext().getProperty('start_stat_bonus');         
+            let basicBonusPoints = this.getView().getModel().getProperty('/Basic_Bonus_Points/0/points');
+            console.log(basicBonusPoints);
+            if(+basicBonusPoints !== 0){
+                this.getView().getModel().setProperty(sPath + '/current_value', sStartValue[0] + (+sStartStatBonus + 1));
+                this.getView().getModel().setProperty(sPath + '/start_stat_bonus', +sStartStatBonus + 1);
+                this.getView().getModel().setProperty('/Basic_Bonus_Points/0/points', +basicBonusPoints-1);}
+            console.log(basicBonusPoints);
+
+        },
+        startStatBonusButtonMinus: function(oEvent) {
+            let sPath = oEvent.getSource().getBindingContext().getPath();
+            let sStartValue = oEvent.getSource().getBindingContext().getProperty('start_value');
+            let limitSumm = 0;
+            for (let i = 0; i<5;i++){
+                limitSumm = limitSumm + (this.getView().getModel().getProperty("/All_Classes/0/table_stats/"+i+"/start_value/0"));
+            }
+            console.log(limitSumm);
             let sStartStatBonus = oEvent.getSource().getBindingContext().getProperty('start_stat_bonus');
-            let sCurrentStat = oEvent.getSource().getBindingContext().getProperty('current_value');
+            let basicBonusPoints = this.getView().getModel().getProperty('/Basic_Bonus_Points/0/points');
+            const limitPoints =+(this.getView().getModel().getProperty('/Basic_Bonus_Points/0/limit'));
+            //console.log(basicBonusPoints);
+            //console.log(limitPoints);
+            if(+basicBonusPoints < limitPoints){
+                this.getView().getModel().setProperty(sPath + '/current_value', sStartValue[0] + (+sStartStatBonus - 1));
+                this.getView().getModel().setProperty(sPath + '/start_stat_bonus', +sStartStatBonus - 1);
+                this.getView().getModel().setProperty('/Basic_Bonus_Points/0/points', +basicBonusPoints+1);
+                limitSumm--
+            }
+            //console.log(oEvent.getSource().getBindingContext().getProperty('start_stat_bonus'));
+            //console.log(sPath + '/current_value', 1 + sStartValue + sStartStatBonus);
 
-            this.getView().getModel().setProperty(sPath + '/current_value', +sStartValue + (+sStartStatBonus + 1));
-            this.getView().getModel().setProperty(sPath + '/start_stat_bonus', +sStartStatBonus + 1);
-
-            console.log(oEvent.getSource().getBindingContext().getProperty('start_stat_bonus'));
-            console.log(sPath + '/current_value', 1 + sStartValue + sStartStatBonus);
+            //console.log(sStartValue);
         },
         onImageLoad: function() {
             //MessageToast.show("Картинка загрузилась");
